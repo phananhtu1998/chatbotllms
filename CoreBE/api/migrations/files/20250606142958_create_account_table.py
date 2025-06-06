ae17110cc2e833d73128275870b9706effda0a32
phananhtu1998 <109066304+phananhtu1998@users.noreply.github.com>
2025-06-06 16:22:57 +0700

    Create 20250606142958_create_account_table.py

---
+++ CoreBE/api/migrations/files/20250606142958_create_account_table.py
@@ -0,0 +1,44 @@
+"""
+Migration: create_account_table
+Description: 
+Created: 2025-06-06T14:29:58.676926
+"""
+
+async def upgrade(conn):
+    """
+    Apply migration changes
+    """
+    # Viết SQL để tạo/thay đổi database schema
+    sql = """
+    CREATE TABLE account (
+    id UUID NOT NULL PRIMARY KEY,
+    number INTEGER NOT NULL,
+    code VARCHAR(255) NOT NULL,
+    name VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    username VARCHAR(255) NOT NULL,
+    password VARCHAR(255) NOT NULL,
+    salt VARCHAR(255) NOT NULL,
+    status BOOLEAN NOT NULL, -- [active,inactive]
+    images VARCHAR(255) NOT NULL,
+    created_by UUID NOT NULL,
+    is_deleted BOOLEAN NOT NULL DEFAULT FALSE,
+    create_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    update_at TIMESTAMP WITHOUT TIME ZONE NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    CONSTRAINT idx_created_by FOREIGN KEY (created_by) REFERENCES account(id)
+    );
+    """
+    
+    await conn.execute(sql)
+
+
+async def downgrade(conn):
+    """
+    Rollback migration changes
+    """
+    # Viết SQL để rollback changes
+    sql = """
+    DROP TABLE IF EXISTS account;
+    """
+    
+    await conn.execute(sql)
