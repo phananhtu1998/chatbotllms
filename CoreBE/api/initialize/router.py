f361134060d07cd32901c0d186f3fa86d931aead
phananhtu1998 <109066304+phananhtu1998@users.noreply.github.com>
2025-06-06 16:26:26 +0700

    Update router.py

--- CoreBE/api/initialize/router.py
+++ CoreBE/api/initialize/router.py
@@ -1,6 +1,7 @@
 from fastapi import APIRouter
 import logging
 from api.router.user.user_router import user_router
+from api.router.account.account_router import account_router
 
 logger = logging.getLogger(__name__)
 
@@ -15,6 +16,11 @@ class RouterInitializer:
                 user_router,
                 prefix="/users",
                 tags=["users"]
+            )
+            self.main_router.include_router(
+                account_router,
+                prefix="/accounts",
+                tags=["accounts"]
             )
             
             logger.info("All routers initialized successfully")
